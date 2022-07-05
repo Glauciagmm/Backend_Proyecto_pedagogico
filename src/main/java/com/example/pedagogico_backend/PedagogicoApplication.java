b21850f71c0a8a727d71c7aba998a08aaa049bc3
Glauciagmm <glauciamesquitagmm@hotmail.com>
2022-07-05 15:40:44 +0200

    first commit

---
+++ src/main/java/com/example/pedagogico_backend/PedagogicoApplication.java
@@ -0,0 +1,13 @@
+package com.example.pedagogico_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PedagogicoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PedagogicoApplication.class, args);
+    }
+
+}
